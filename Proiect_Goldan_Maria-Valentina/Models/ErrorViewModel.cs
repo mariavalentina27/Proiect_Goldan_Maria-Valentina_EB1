da6856f387e0040efcd073ce012e4561704cbe09
Maria s Computer <Maria s Computer@MARIA>
2024-01-07 13:25:04 +0200

    Add project files.

---
+++ Proiect_Goldan_Maria-Valentina/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proiect_Goldan_Maria_Valentina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
